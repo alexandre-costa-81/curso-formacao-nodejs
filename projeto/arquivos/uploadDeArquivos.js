5fa9e695f8426eef80fc21ad419561904de86bf0
Alexandre Costa <alexandre.costa@ufpel.edu.br>
2021-09-25 14:44:28 -0300

    Stream

--- projeto/arquivos/uploadDeArquivos.js
+++ projeto/arquivos/uploadDeArquivos.js
@@ -1,18 +1,22 @@
 const fs = require('fs')
 
-fs.readFile('./assets/nery.jpg', (erro, buffer) => {
-    if (erro) {
-        console.log(erro)
-    } else {
-        console.log('A imagem foi bufferizada hehehe')
-        console.log(buffer)
+// fs.readFile('./assets/nery.jpg', (erro, buffer) => {
+//     if (erro) {
+//         console.log(erro)
+//     } else {
+//         console.log('A imagem foi bufferizada hehehe')
+//         console.log(buffer)
 
-        fs.writeFile('./assets/nery2.jpg', buffer, (erro) => {
-            if (erro) {
-                console.log(erro)
-            } else {
-                console.log('A imagem foi escrita')
-            }
-        })
-    }
-})
+//         fs.writeFile('./assets/nery2.jpg', buffer, (erro) => {
+//             if (erro) {
+//                 console.log(erro)
+//             } else {
+//                 console.log('A imagem foi escrita')
+//             }
+//         })
+//     }
+// })
+
+fs.createReadStream('./assets/nery.jpg')
+    .pipe(fs.createWriteStream('./assets/nery-stream.jpg'))
+    .on('finish', () => console.log('Foto salva com sucesso'))
