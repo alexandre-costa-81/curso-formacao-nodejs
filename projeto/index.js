102a90f8c83c8b3d7e69daa21d94236fa15223f5
Alexandre Costa <alexandre.costa@ufpel.edu.br>
2021-09-24 22:52:03 -0300

    Usando o nodemon

--- projeto/index.js
+++ projeto/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const express = require('express')
 
-const app = express();
+const app = express()
 
-app.listen(3000, () => console.log('servidor rodando na porta 3000'));
+app.listen(3000, () => console.log('servidor rodando na porta 3000'))
 
 app.get('/atendimentos', (req, res) => res.send('Vocês está na rota de atendimentos'))
