a7e1bc4eb8e94359fa1b67cacacb2a6a2439dcf4
Alexandre Costa <alexandre.costa@ufpel.edu.br>
2021-09-25 00:27:24 -0300

    Criando nossa base de dados com MySQL

--- projeto/index.js
+++ projeto/index.js
@@ -1,5 +1,13 @@
 const customExpres = require('./config/customExpress')
+const conexao = require('./infraestrutura/conexao')
 
-app = customExpres()
+conexao.connect(error => {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log('conectado com sucesso')
+        app = customExpres()
+        app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+    }
+})
 
-app.listen(3000, () => console.log('servidor rodando na porta 3000'))
