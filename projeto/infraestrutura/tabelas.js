a8e5ced04d17eeb5618ea83258a7636f2cefdbaa
Alexandre Costa <alexandre.costa@ufpel.edu.br>
2021-09-25 01:29:10 -0300

    Adicionando data no atendimento

--- projeto/infraestrutura/tabelas.js
+++ projeto/infraestrutura/tabelas.js
@@ -6,7 +6,7 @@ class Tabelas {
     }
 
     criateAtendimentos() {
-        const sql = 'CREATE TABLE atendimentos (id int NOT NULL AUTO_INCREMENT, cliente VARCHAR(50) NOT NULL, pet VARCHAR(20), servico VARCHAR(50) NOT NULL, status VARCHAR(20) NOT NULL, observacoes TEXT, PRIMARY KEY(id))'
+        const sql = 'CREATE TABLE atendimentos (id int NOT NULL AUTO_INCREMENT, cliente VARCHAR(50) NOT NULL, pet VARCHAR(20), servico VARCHAR(50) NOT NULL, data datetime NOT NULL, dataCriacao datetime NOT NULL, status VARCHAR(20) NOT NULL, observacoes TEXT, PRIMARY KEY(id))'
 
         this.conexao.query(sql, (error) => {
             if (error) {
