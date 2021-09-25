15bbe3a466544e7582c44ddc10b3491791ff3437
Alexandre Costa <alexandre.costa@ufpel.edu.br>
2021-09-24 23:21:13 -0300

    Rota para criar atendimento

--- projeto/controllers/atendimentos.js
+++ projeto/controllers/atendimentos.js
@@ -1,3 +1,5 @@
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('Vocês está na rota de atendimentos'))
+    app.get('/atendimentos', (req, res) => res.send('Vocês está na rota de atendimentos e está fazendo um GET'))
+
+    app.post('/atendimentos', (req, res) => res.send('Vocês está na rota de atendimentos e está fazendo um POST'))
 }
