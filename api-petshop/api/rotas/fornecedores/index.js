e7d3be9eb87666067ef9f6fe7ca3cdaf7f3b5896
Alexandre Costa <alexandre.costa@ufpel.edu.br>
2021-09-26 14:57:32 -0300

    Persistindo dados

--- api-petshop/api/rotas/fornecedores/index.js
+++ api-petshop/api/rotas/fornecedores/index.js
@@ -1,11 +1,19 @@
 const roteador = require('express').Router()
 const TabelaFornecedor = require('./TabelaFornecedor')
+const Fornecedor = require('./Fornecedor')
 
-roteador.use('/', async (req, res) => {
+roteador.get('/', async (req, res) => {
     const resultado = await TabelaFornecedor.listar()
     res.send(
         JSON.stringify(resultado)
     )
+})
+
+roteador.post('/', async (req, res) => {
+    const dadosRecebidos = req.body
+    const fornecedor = new Fornecedor(dadosRecebidos)
+    await fornecedor.criar()
+    res.send(JSON.stringify(fornecedor))
 })
 
 module.exports = roteador
