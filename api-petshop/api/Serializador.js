49d4245bda54a4eae2cf5504150c2bf5d707b583
Alexandre Costa <alexandre.costa@ufpel.edu.br>
2021-09-26 20:58:13 -0300

    Processando XML no Serializador

--- api-petshop/api/Serializador.js
+++ api-petshop/api/Serializador.js
@@ -1,15 +1,33 @@
-const ValorNaoSuportado = require("./erros/ValorNaoSuportado")
-
+const ValorNaoSuportado = require('./erros/ValorNaoSuportado')
+const jsontoxml = require('jsontoxml')
 class Serializador {
     json(dados) {
         return JSON.stringify(dados)
     }
 
+    xml(dados) {
+        let tag = this.tagSingular
+
+        if (Array.isArray(dados)) {
+            tag = this.tagPlural
+            dados = dados.map((item) => {
+                return {
+                    [this.tagSingular]: item
+                }
+            })
+        }
+
+        return jsontoxml({ [tag]: dados })
+    }
+
     serializar(dados) {
+        dados = this.filtrar(dados)
         if (this.contentType === 'application/json') {
-            return this.json(
-                this.filtrar(dados)
-            )
+            return this.json(dados)
+        }
+
+        if (this.contentType === 'application/xml') {
+            return this.xml(dados)
         }
 
         throw new ValorNaoSuportado(this.contentType)
@@ -49,6 +67,8 @@ class SerializadorFornecedor extends Serializador {
             'empresa',
             'categoria'
         ].concat(camposExtras || [])
+        this.tagSingular = 'fornecedor'
+        this.tagPlural = 'fornecedores'
     }
 }
 
@@ -57,6 +77,8 @@ class SerializadorErro extends Serializador {
         super()
         this.contentType = contentType
         this.camposPublico = ['id', 'mensagem'].concat(camposExtras || [])
+        this.tagSingular = 'erro'
+        this.tagPlural = 'erros'
     }
 }
 
@@ -64,5 +86,5 @@ module.exports = {
     Serializador: Serializador,
     SerializadorFornecedor: SerializadorFornecedor,
     SerializadorErro: SerializadorErro,
-    formatosAceitos: ['application/json']
+    formatosAceitos: ['application/json', 'application/xml']
 }
