22a685d5acbce426b64642e5d812bb4d8caf9983
Alexandre Costa <alexandre.costa@ufpel.edu.br>
2021-09-26 17:25:58 -0300

    Declarando sucesso na resposta

--- api-petshop/api/rotas/fornecedores/index.js
+++ api-petshop/api/rotas/fornecedores/index.js
@@ -4,6 +4,7 @@ const Fornecedor = require('./Fornecedor')
 
 roteador.get('/', async (req, res) => {
     const resultado = await TabelaFornecedor.listar()
+    res.status(200)
     res.send(
         JSON.stringify(resultado)
     )
@@ -14,6 +15,7 @@ roteador.post('/', async (req, res) => {
         const dadosRecebidos = req.body
         const fornecedor = new Fornecedor(dadosRecebidos)
         await fornecedor.criar()
+        res.status(201)
         res.send(JSON.stringify(fornecedor))
     } catch (erro) {
         res.send(JSON.stringify({
@@ -27,6 +29,7 @@ roteador.get('/:id', async (req, res) => {
         const id = req.params.id
         const fornecedor = new Fornecedor({ id: id })
         await fornecedor.carregar()
+        res.status(200)
         res.send(JSON.stringify(fornecedor))
     } catch (erro) {
         res.send(JSON.stringify({
@@ -42,6 +45,7 @@ roteador.put('/:id', async (req, res) => {
         const dados = Object.assign({}, dadosRecebidos, { id: id })
         const fornecedor = new Fornecedor(dados)
         await fornecedor.atualizar()
+        res.status(204)
         res.end()
     } catch (erro) {
         res.send(JSON.stringify({
@@ -56,6 +60,7 @@ roteador.delete('/:id', async (req, res) => {
         const fornecedor = new Fornecedor({id: id })
         await fornecedor.carregar()
         await fornecedor.remover()
+        res.status(204)
         res.end()
     } catch (erro) {
         res.send(JSON.stringify({
